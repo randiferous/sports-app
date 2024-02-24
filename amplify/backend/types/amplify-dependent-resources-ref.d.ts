37a71109c2db68dfaf678ff60023627983ff9c1a
Daniel Y. Lee <daniel.younghwan.lee@gmail.com>
2024-02-23 22:27:19 -0500

    added database integration

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,5 +1,10 @@
 export type AmplifyDependentResourcesAttributes = {
   "api": {
+    "islandersdb": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    },
     "sportsappBackend": {
       "ApiId": "string",
       "ApiName": "string",
@@ -13,6 +18,25 @@ export type AmplifyDependentResourcesAttributes = {
       "LambdaExecutionRoleArn": "string",
       "Name": "string",
       "Region": "string"
+    },
+    "sportsapp515e7511": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "islandersdb": {
+      "Arn": "string",
+      "Name": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "Region": "string",
+      "SortKeyName": "string",
+      "SortKeyType": "string",
+      "StreamArn": "string"
     }
   }
 }
--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 
 function App() {
   const [teamData, setTeamData] = useState({ forwards: [], defensemen: [], goalies: [] });
+  const [funFacts, setFunFacts] = useState([]);
 
   useEffect(() => {
     // Replace 'YOUR_BACKEND_URL' with the actual backend URL
-    const fetchData = async () => {
+    const fetchTeamData = async () => {
       try {
         const response = await fetch('https://e3oq1j7ufg.execute-api.us-east-2.amazonaws.com/dev/stats');
         if (!response.ok) {
@@ -19,13 +20,31 @@ function App() {
       }
     };
 
-    fetchData();
+    const fetchFunFacts = async () => {
+      try {
+        const response = await fetch(' https://6r7hy9ope9.execute-api.us-east-2.amazonaws.com/dev/funfacts');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        setFunFacts(data);
+      } catch (error) {
+        console.error('Error fetching fun facts:', error);
+      }
+    };
+
+    fetchTeamData();
+    fetchFunFacts();
   }, []);
 
   const renderPlayer = (player) => (
     <li key={player.id}>
       {player.firstName.default} {player.lastName.default} - #{player.sweaterNumber} - {player.positionCode}
     </li>
+  );
+
+  const renderFact = (fact) => (
+    <li key={fact.id}>{fact.fact}</li>
   );
 
   return (
@@ -43,6 +62,10 @@ function App() {
         <h3>Goalies</h3>
         <ul>
           {teamData.goalies.map(renderPlayer)}
+        </ul>
+        <h2>Fun Facts</h2>
+        <ul>
+          {funFacts.map(renderFact)}
         </ul>
       </header>
     </div>
